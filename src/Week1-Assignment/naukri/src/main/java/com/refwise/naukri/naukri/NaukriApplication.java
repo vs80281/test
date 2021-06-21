874bae051c1ae7c26f9183cd18d6fb1ec7adef00
Kumar <amit.4.kumar@atos.net>
2021-06-21 20:22:15 +0530

    assignment one

---
+++ src/Week1-Assignment/naukri/src/main/java/com/refwise/naukri/naukri/NaukriApplication.java
@@ -0,0 +1,14 @@
+package com.refwise.naukri.naukri;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NaukriApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NaukriApplication.class, args);
+		System.out.println("Hello World");
+	}
+
+}
---
+++ src/Week1-Assignment/naukri/src/test/java/com/refwise/naukri/naukri/NaukriApplicationTests.java
@@ -0,0 +1,13 @@
+package com.refwise.naukri.naukri;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NaukriApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
